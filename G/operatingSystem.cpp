6f096d9977eccb37db3166d20a7fe876eeee5864
Shicheng Chen <c337134154@gmail.com>
2021-04-12 11:29:38 +0800

    add merge to buddy allocator

--- G/operatingSystem.cpp
+++ G/operatingSystem.cpp
@@ -17,8 +17,10 @@ typedef long long ll;
 __FILE__,__LINE__,__func__,#x,x,#y,y,#z,z);
 #define debug4(x,y,z,h) printf("%s:%d:%s(): ->%s:%d ->%s:%d ->%s:%d ->%s:%d\n", \
 __FILE__,__LINE__,__func__,#x,x,#y,y,#z,z,#h,h);
-#define GET4(a, b, c, d,e, ...) e
-#define d_bug(...) GET4(__VA_ARGS__, debug4, debug3, debug2, debug1)
+#define debug5(x,y,z,h,i) printf("%s:%d:%s(): ->%s:%d ->%s:%d ->%s:%d ->%s:%d ->%s:%d\n", \
+__FILE__,__LINE__,__func__,#x,x,#y,y,#z,z,#h,h,#i,i);
+#define GET6(a, b, c, d,e,f, ...) f
+#define d_bug(...) GET6(__VA_ARGS__, debug5, debug4, debug3, debug2, debug1)
 #define debug(...) d_bug(__VA_ARGS__)(__VA_ARGS__)
 struct LRUCache {
 public:
@@ -139,6 +141,11 @@ struct Vector{
     int pop_back(int i){check(i);return vec[i][--cnt[i]];}
     int sz(int i){check(i); return cnt[i];}
     void clear(){memset(cnt,0,sizeof cnt);}
+    int checkSz(){
+        int ans=0;
+        FOR(5)ans+=(1<<i)*cnt[i];
+        return ans;
+    }
 };
 struct BuddyList{
     Vector v;
@@ -153,49 +160,66 @@ struct BuddyList{
         //initialize all blocks to one
         FOR(n)arr[i]=1;
         FOR(n)v.push_back(0,i);
+        assert(PAGENUM==v.checkSz()+cnt);
         cnt=0;
-        //merge();
+        merge();
     }
     void merge(){
         //time complexity is 2n=n+n/2+n/4+n/8+n/16
         //the worst time complexity is 5n
-        memset(maxn,0,sizeof maxn);
+        //memset(maxn,0,sizeof maxn);
         for (int k = 2; k <= 16; k*=2)
             for (int i = 0; i < n; i+=k)
                 if(arr[i]==k/2 && arr[i+k/2]==k/2){
-                    FOR(_,i,i+k)arr[_]=k,maxn[_]=0;
-                    maxn[i]=k;
+                    arr[i]=k,arr[i+k/2]=0;
+                    FOR(_,i+1,i+k)assert(arr[_]==0);
+                    //arr[i]=maxn[i]=k;
                 }
-        FOR(n)if(maxn[i])v.push_back(ma[maxn[i]],i);
+        v.clear();
+        //FOR(n)if(maxn[i])v.push_back(ma[maxn[i]],i);
+        FOR(n)if(arr[i]>0)v.push_back(ma[arr[i]],i);
+        assert(PAGENUM==v.checkSz()+cnt);
+    }
+    bool continousMalloc(int &msize,int *address,int &cadd){
+        if(!msize)return true;
+        int order=-1;
+        FOR(i,ma[msize],5)if(v.sz(i)){order=i;break;}
+        if(order==-1)return false;
+        int block=(1<<order);
+        //int left=(1<<order)-msize;
+        int idx=v.pop_back(order);
+        puts("continous");
+        debug(order,block,idx,v.checkSz())
+        //int endx=idx+(1<<order);
+        assert(arr[idx]==block);
+        FOR(_,idx,idx+msize)arr[_]=-1,address[cadd++]=_,cnt++;
+        FOR(_,idx+msize,idx+block)v.push_back(0,_),arr[_]=1;//, debug(v.checkSz());
+        msize=0;
+        return true;
+    }
+    void discontinuousMalloc(int &msize,int *address,int &cadd){
+        if(!msize)return;
+        puts("discontinuous");
+        for (int i = 4; i >= 0; --i) {
+            int block=(1<<i);
+            debug(i,v.sz(i));
+            while(msize-block>=0 && v.sz(i)){
+                debug(i,msize,v.sz(i));
+                msize-=block;
+                int idx=v.pop_back(i);
+                FOR(_,idx,idx+block)arr[_]=-1,address[cadd++]=_,cnt++;
+            }
+        }
     }
     void malloc(int msize,int *address){
         int cadd=0,osize=msize;
-        int order=-1;
-        FOR(i,ma[msize],5)if(v.sz(i)){order=i;break;}
-        //if(order==-1)merge();
-        FOR(i,ma[msize],5)if(v.sz(i)){order=i;break;}
-        if(order!=-1){
+        if(!continousMalloc(msize,address,cadd))merge();
+        continousMalloc(msize,address,cadd);
+        discontinuousMalloc(msize,address,cadd);
+        continousMalloc(msize,address,cadd);
 
-            //continous malloc
-            int block=(1<<order);
-            //int left=(1<<order)-msize;
-            int idx=v.pop_back(order);
-            //int endx=idx+(1<<order);
-            FOR(_,idx,idx+msize)arr[_]=-1,address[cadd++]=_,cnt++;
-            FOR(_,idx+msize,idx+block)v.push_back(0,_),arr[_]=1;
-        }else{
-            //discontinuous malloc
-            for (int i = 3; i >= 0; --i) {
-                int block=(1<<i);
-                while(msize-block>=0 && v.sz(i)){
-                    //debug(i,msize,v.sz(i));
-                    msize-=block;
-                    int idx=v.pop_back(i);
-                    FOR(_,idx,idx+block)arr[_]=-1,address[cadd++]=_,cnt++;
-                }
-            }
-        }
-        //debug(cadd,osize,cnt)
+        debug(msize,cadd,osize,cnt,v.checkSz())
+        assert(PAGENUM==v.checkSz()+cnt);
         assert(cadd==osize);
     }
     void free(int idx){
@@ -205,7 +229,7 @@ struct BuddyList{
 
         cnt--;
         v.push_back(0,idx);//since no merge, add directly
-        //merge();
+        assert(PAGENUM-cnt==v.checkSz());
     }
 };
 LRUCache active(250),inactive(250);
